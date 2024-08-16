02bf11dfea0eb1c4a16a2c8397d696ccdcb2302c
NguyenDinhPhi2005 <23633271.phi@student.iuh.edu.vn>
2024-08-16 11:08:17 +0700

    Welcome

---
+++ src/Welcome.java
@@ -0,0 +1,6 @@
+public class Welcome {
+    public static void main(String[] args) {
+        System.out.println("Welcome Nguyen Dinh Phi!");
+        System.out.println("Welcome to the DHKTPM19ATT!");
+    }
+}
